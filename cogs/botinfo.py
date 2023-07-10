9bc58c618e584d83c1fecebbe4ce5f69cf628585
Zerbaib <hugo.hennetin@proton.me>
2023-07-10 11:09:19 +0200

    🔃 color to 🟥

---
+++ cogs/botinfo.py
@@ -0,0 +1,32 @@
+import disnake
+from disnake.ext import commands
+
+class botinfoCommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'🔩 /botinfo as been loaded')
+    
+    @commands.slash_command(name="botinfo", description="Get the bot's info",)
+    async def ping(self, ctx):
+        try:
+            embed = disnake.Embed(
+                title=f"Info of {self.bot.name}",
+                description=f"The ping is around `{round(self.bot.latency * 1000)}ms`",
+                color=disnake.Color.random()
+                )
+            embed.set_footer(text=f'Command executed by {ctx.author}', icon_url=ctx.author.avatar.url)
+            await ctx.response.send_message(ephemeral=True, embed=embed)
+        except Exception as e:
+            embed = disnake.Embed(
+                title=f"Error during the ``/ping``",
+                description=f"```{e}```",
+                color=disnake.Color.random()
+                )
+            embed.set_footer(text=f'Command executed by {ctx.author}', icon_url=ctx.author.avatar.url)
+            await ctx.response.send_message(embed=embed)
+
+def setup(bot):
+    bot.add_cog(botinfoCommand(bot))
