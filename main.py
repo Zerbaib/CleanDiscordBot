195ae38e29c2fbf00bd60c23b97a2ef4c18afd08
Zerbaib <hugo.hennetin@proton.me>
2023-07-10 17:48:25 +0200

    ✅ Status loop

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ import json
 import os
 import platform
 import requests
+import asyncio
 
 config_file_path = "config.json"
 online_version = "https://raw.githubusercontent.com/Zerbaib/CleanDiscordBot/main/version.txt"
+status_messages = [
+    {"name": "Version", "value": "", "type": disnake.ActivityType.watching},
+    {"name": "Users", "value": "", "type": disnake.ActivityType.listening},
+    {"name": "Commands", "value": "", "type": disnake.ActivityType.playing}
+]
 
 if not os.path.exists(config_file_path):
     with open(config_file_path, 'w') as config_file:
@@ -36,6 +42,7 @@ bot = commands.Bot(
     intents=disnake.Intents.all(),
     case_insensitive=True
 )
+bot.remove_command('help')
 
 @bot.event
 async def on_ready():
@@ -68,6 +75,30 @@ async def on_ready():
     print(f"🔱 Running on {platform.system()} {platform.release()} {os.name}")
     print(f"🔱 Python version: {platform.python_version()}")
     print('===============================================')
+    bot.loop.create_task(update_status())
+
+async def update_status():
+    while True:
+        for status in status_messages:
+            if status["name"] == "Version":
+                with open('version.txt', 'r') as version_file:
+                    bot_version = version_file.read().strip()
+                status["value"] = f"{bot_version}"
+            elif status["name"] == "Users":
+                status["value"] = f"{len(bot.users)} users"
+            elif status["name"] == "Commands":
+                status["value"] = f"{len(bot.slash_commands)} commands"
+
+        current_status = status_messages.pop(0)
+        status_messages.append(current_status)
+
+        await bot.change_presence(
+            activity=disnake.Activity(
+                type=current_status["type"],
+                name=current_status["value"]
+            )
+        )
+        await asyncio.sleep(4)
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
