66230bb3d8c3d0daae15f74210f9afed4a80c06a
Zerbaib <hugo.hennetin@proton.me>
2023-07-13 01:15:05 +0200

    ⤴️ Patch

--- cogs/botinfo.py
+++ cogs/botinfo.py
@@ -1,15 +1,15 @@
 import disnake
 from disnake.ext import commands
-import platform
 import requests
+import platform
 
-class botinfoCommand(commands.Cog):
+class BotInfoCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print(f'🔩 /botinfo has been loaded')
+        print('🔩 /botinfo has been loaded')
     
     @commands.slash_command(name="botinfo", description="Get the bot's info")
     async def botinfo(self, ctx):
@@ -19,6 +19,7 @@ class botinfoCommand(commands.Cog):
             
             github_repo = "https://github.com/Zerbaib/CleanDiscordBot"
             online_version_url = "https://raw.githubusercontent.com/Zerbaib/CleanDiscordBot/main/version.txt"
+            github_api_url = "https://api.github.com/repos/Zerbaib/CleanDiscordBot"
             
             response = requests.get(online_version_url)
             if response.status_code == 200:
@@ -26,22 +27,35 @@ class botinfoCommand(commands.Cog):
             else:
                 online_version = "Unknown"
 
-            response = requests.get(f'{github_repo}/commits')
-            if response.status_code == 200 and response.headers.get('content-type') == 'application/json':
-                commit_count = len(response.json())
+            response = requests.get(f"{github_api_url}/commits?per_page=100&sha=main")
+            if response.status_code == 200:
+                commits = response.json()
+                commit_count = len(commits)
+                while "Link" in response.headers and "rel=\"next\"" in response.headers["Link"]:
+                    next_page_url = response.headers["Link"].split(";")[0][1:-1]
+                    response = requests.get(next_page_url)
+                    if response.status_code == 200:
+                        commits += response.json()
+                        commit_count += len(response.json())
             else:
                 commit_count = "Unknown"
             
-            response = requests.get(f'{github_repo}/stargazers')
-            if response.status_code == 200 and response.headers.get('content-type') == 'application/json':
-                stargazer_count = len(response.json())
+            response = requests.get(f"{github_api_url}/stargazers")
+            if response.status_code == 200:
+                stargazers = response.json()
+                stargazer_count = len(stargazers)
+                while "Link" in response.headers and "rel=\"next\"" in response.headers["Link"]:
+                    next_page_url = response.headers["Link"].split(";")[0][1:-1]
+                    response = requests.get(next_page_url)
+                    if response.status_code == 200:
+                        stargazers += response.json()
+                        stargazer_count += len(response.json())
             else:
                 stargazer_count = "Unknown"
 
             embed = disnake.Embed(
                 title=f"Info of ``{self.bot.user.name}``",
-                description=f"",
-                color=disnake.Color.random()
+                color=disnake.Color.old_blurple()
             )
             embed.add_field(
                 name=f"__Bot profile:__",
@@ -52,8 +66,8 @@ class botinfoCommand(commands.Cog):
             )
             embed.add_field(
                 name=f"__Bot info:__",
-                value=f"**Version**: ``{bot_version}``\n"
-                      f"**Api version**: ``{disnake.__version__}``\n"
+                value=f"**Bot version**: ``{bot_version}``\n"
+                      f"**API version**: ``{disnake.__version__}``\n"
                       f"**Python version**: ``{platform.python_version()}``",
                 inline=False
             )
@@ -61,12 +75,13 @@ class botinfoCommand(commands.Cog):
                 name=f"__GitHub Repository:__",
                 value=f"**Commits**: ``{commit_count}``\n"
                       f"**Stars**: ``{stargazer_count}``\n"
-                      f"**Version**: ``{online_version}``",
+                      f"**Online Version**: ``{online_version}``",
                 inline=False
             )
             embed.set_thumbnail(url=self.bot.user.avatar.url)
             embed.set_footer(text=f'Command executed by {ctx.author}', icon_url=ctx.author.avatar.url)
-            await ctx.response.send_message(ephemeral=True, embed=embed)
+            
+            await ctx.response.send_message(embed=embed)
         except Exception as e:
             embed = disnake.Embed(
                 title=f"Error during the ``/botinfo``",
@@ -77,4 +92,4 @@ class botinfoCommand(commands.Cog):
             await ctx.response.send_message(embed=embed)
 
 def setup(bot):
-    bot.add_cog(botinfoCommand(bot))
+    bot.add_cog(BotInfoCommand(bot))
