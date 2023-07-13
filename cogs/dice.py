0eacb8831187db5405ccc4d313fa0380aabd0725
Zerbaib <hugo.hennetin@proton.me>
2023-07-13 13:31:13 +0200

    🔝 Patch

--- cogs/dice.py
+++ cogs/dice.py
@@ -18,32 +18,39 @@ class DiceCommand(commands.Cog):
         with open(self.data_file, 'r') as file:
             data = json.load(file)
         
-        dice_emojis = [':one:', ':two:', ':three:', ':four:', ':five:', ':six:']
-        dice1 = random.randint(1, 6)
-        dice2 = random.randint(1, 6)
+        bal = data[user_id]
 
-        payout = 0
-
-        if dice1 == dice2:  # Pair
-            payout = bet * dice1
-
-        embed = disnake.Embed(title="Dice Game", color=disnake.Color.blue())
-        embed.add_field(name="Dice Roll Result", value=f"{dice_emojis[dice1 - 1]}  {dice_emojis[dice2 - 1]}", inline=False)
-
-        if payout > 0:
-            data[user_id] += payout
-            embed.add_field(name="Result", value=f"You won `{payout}` coin!")
-            embed.color = disnake.Color.green()
+        if bal < bet:
+            embed = disnake.Embed(title="Dice Game", color=disnake.Color.red())
+            embed.add_field(name="You cant play !", value=f"You don't have money to play ``{bet}`` coin try with less.", inline=False)
+            await ctx.send(embed=embed)
         else:
-            data[user_id] -= bet
-            embed.add_field(name="Bet", value=f"`{bet}`")
-            embed.add_field(name="Result", value="You lost your bet.")
-            embed.color = disnake.Color.red()
+            dice_emojis = [':one:', ':two:', ':three:', ':four:', ':five:', ':six:']
+            dice1 = random.randint(1, 6)
+            dice2 = random.randint(1, 6)
 
-        with open(self.data_file, 'w') as file:
-            json.dump(data, file, indent=4)
+            payout = 0
 
-        await ctx.send(embed=embed)
+            if dice1 == dice2:  # Pair
+                payout = bet * dice1
+
+            embed = disnake.Embed(title="Dice Game", color=disnake.Color.blue())
+            embed.add_field(name="Dice Roll Result", value=f"{dice_emojis[dice1 - 1]}  {dice_emojis[dice2 - 1]}", inline=False)
+
+            if payout > 0:
+                data[user_id] += payout
+                embed.add_field(name="Result", value=f"You won `{payout}` coin!")
+                embed.color = disnake.Color.green()
+            else:
+                data[user_id] -= bet
+                embed.add_field(name="Bet", value=f"`{bet}`")
+                embed.add_field(name="Result", value="You lost your bet.")
+                embed.color = disnake.Color.red()
+
+            with open(self.data_file, 'w') as file:
+                json.dump(data, file, indent=4)
+
+            await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(DiceCommand(bot))
