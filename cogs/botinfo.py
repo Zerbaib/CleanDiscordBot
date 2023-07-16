fae4d1e6a8913faa3909d1bb41161e4c026e51f5
Zerbaib <hugo.hennetin@proton.me>
2023-07-17 00:55:33 +0200

    🔀 change to aiohttp

--- cogs/botinfo.py
+++ cogs/botinfo.py
@@ -1,6 +1,6 @@
 import disnake
 from disnake.ext import commands
-import requests
+import aiohttp
 import platform
 
 class BotInfoCommand(commands.Cog):
@@ -20,38 +20,41 @@ class BotInfoCommand(commands.Cog):
             github_repo = "https://github.com/Zerbaib/CleanDiscordBot"
             online_version_url = "https://raw.githubusercontent.com/Zerbaib/CleanDiscordBot/main/version.txt"
             github_api_url = "https://api.github.com/repos/Zerbaib/CleanDiscordBot"
-            
-            response = requests.get(online_version_url)
-            if response.status_code == 200:
-                online_version = response.text.strip()
-            else:
-                online_version = "Unknown"
 
-            response = requests.get(f"{github_api_url}/commits?per_page=100&sha=main")
-            if response.status_code == 200:
-                commits = response.json()
-                commit_count = len(commits)
-                while "Link" in response.headers and "rel=\"next\"" in response.headers["Link"]:
-                    next_page_url = response.headers["Link"].split(";")[0][1:-1]
-                    response = requests.get(next_page_url)
-                    if response.status_code == 200:
-                        commits += response.json()
-                        commit_count += len(response.json())
-            else:
-                commit_count = "Unknown"
-            
-            response = requests.get(f"{github_api_url}/stargazers")
-            if response.status_code == 200:
-                stargazers = response.json()
-                stargazer_count = len(stargazers)
-                while "Link" in response.headers and "rel=\"next\"" in response.headers["Link"]:
-                    next_page_url = response.headers["Link"].split(";")[0][1:-1]
-                    response = requests.get(next_page_url)
-                    if response.status_code == 200:
-                        stargazers += response.json()
-                        stargazer_count += len(response.json())
-            else:
-                stargazer_count = "Unknown"
+            async with aiohttp.ClientSession() as session:
+                async with session.get(online_version_url) as response:
+                    if response.status == 200:
+                        online_version = await response.text()
+                    else:
+                        online_version = "Unknown"
+
+                async with session.get(f"{github_api_url}/commits?per_page=100&sha=main") as response:
+                    if response.status == 200:
+                        commits = []
+                        while True:
+                            commits.extend(await response.json())
+                            link_header = response.headers.get('Link')
+                            if not link_header or 'rel="next"' not in link_header:
+                                break
+                            next_page_url = link_header.split(";")[0][1:-1]
+                            async with session.get(next_page_url) as response:
+                                if response.status != 200:
+                                    break
+                    commit_count = len(commits) if commits else "Unknown"
+
+                async with session.get(f"{github_api_url}/stargazers") as response:
+                    if response.status == 200:
+                        stargazers = []
+                        while True:
+                            stargazers.extend(await response.json())
+                            link_header = response.headers.get('Link')
+                            if not link_header or 'rel="next"' not in link_header:
+                                break
+                            next_page_url = link_header.split(";")[0][1:-1]
+                            async with session.get(next_page_url) as response:
+                                if response.status != 200:
+                                    break
+                    stargazer_count = len(stargazers) if stargazers else "Unknown"
 
             embed = disnake.Embed(
                 title=f"Info of ``{self.bot.user.name}``",
