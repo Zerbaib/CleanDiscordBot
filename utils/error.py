1044fc59f99aa42ed8029607ab0b1b7be13f7668
Hennetin Hugo <zerbaib@proton.me>
2023-07-31 22:49:03 +0200

    🗡 Error patcher

---
+++ utils/error.py
@@ -0,0 +1,24 @@
+import disnake
+
+def create_error_embed(title: str, description: str):
+    """
+    Create an embed with an error message.
+
+    Parameters:
+        title (str): The title of the error embed.
+        description (str): The description of the error embed.
+
+    Returns:
+        disnake.Embed: The error embed.
+    """
+    issue_link = "https://github.com/Zerbaib/CleanDiscordBot/issues/new"
+    embed = disnake.Embed(
+        title=title,
+        description=description,
+        color=disnake.Color.red()
+    )
+    embed.add_field(
+        name="You can now create a Issue on GitHub",
+        description=f"Tell us wath command and the exeption [**here**]({issue_link})"
+    )
+    return embed
