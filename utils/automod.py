8b1d6b4803f4fa32ab19b950a69937334ad36bf1
Hennetin Hugo <zerbaib@proton.me>
2023-08-05 17:49:29 +0200

    ⛔️ Anti link as been here

--- utils/automod.py
+++ utils/automod.py
@@ -12,6 +12,7 @@ class AutoModUtils(commands.Cog):
         with open('bad_words.json', 'r') as bad_words_file:
             self.bad_words = json.load(bad_words_file)["bad_words"]
             self.bad_word_patterns = [re.compile(rf'\b{re.escape(word)}\b', re.IGNORECASE) for word in self.bad_words]
+            self.link_pattern = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -31,7 +32,7 @@ class AutoModUtils(commands.Cog):
                     embed = disnake.Embed(
                         title=f"{message.author.display_name} -> {word}",
                         description=f"User has posted a bad word:\n```{word}```",
-                        color=disnake.Color.red()
+                        color=disnake.Color.brand_red()
                     )
                     embed.add_field(
                         name="Go to message",
@@ -40,6 +41,25 @@ class AutoModUtils(commands.Cog):
                     log_channel_id = config["LOG_ID"]
                     log_channel = self.bot.get_channel(log_channel_id)
                     await log_channel.send(embed=embed)
+            
+            if self.link_pattern.search(message.content):
+                log_channel = self.bot.get_channel(config["LOG_ID"])
+                if log_channel:
+                    embed = disnake.Embed(title="Link Detected",
+                                        description=f"A link was fund from {message.author.mention}.",
+                                        color=disnake.Color.brand_red())
+                    embed.add_field(name="Contenu du Message", value=message.content, inline=False)
+                    await log_channel.send(embed=embed)
+                user_embed = disnake.Embed(title="Warning",
+                                        description=f"Your message ```{message.content}``` as been blocked.",
+                                        color=disnake.Color.brand_red())
+                await message.author.send(embed=user_embed)
+                await message.delete()
+
+                temp_embed = disnake.Embed(
+                    description=f"A link as been blocker.\nPlease don't send link {message.author.mention}",
+                    color=disnake.Color.brand_red())
+                await message.channel.send(embed=temp_embed, delete_after=10.0)
 
 def setup(bot):
     bot.add_cog(AutoModUtils(bot))
