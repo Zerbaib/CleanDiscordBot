05b6a958f0186a738c696e0b5e7bc42d4b30e268
Hennetin Hugo <zerbaib@proton.me>
2023-08-24 08:58:17 +0200

    💢 IDK

--- cogs/money.py
+++ cogs/money.py
@@ -1,73 +1,48 @@
 import disnake
 from disnake.ext import commands
-import yfinance as yf
-import matplotlib.pyplot as plt
+import finnhub
 import io
-from difflib import get_close_matches
-from utils import error
+import matplotlib.pyplot as plt
+from utils import error  # Importer la fonction d'erreur personnalisée
+
+# Initialiser la clé API Finnhub
+finnhub_client = finnhub.Client(api_key="")
 
 class TradeViewCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(name="tradeview", description="Get stock price information from Yahoo Finance.")
+    @commands.slash_command(name="tradeview", description="Get stock price information from Finnhub.")
     async def tradeview(self, ctx, choice: str):
         try:
-            await ctx.response.defer()
+            # Obtenir les données d'historique des prix à partir de l'API Finnhub
+            res = finnhub_client.stock_candles(choice, 'D', 30, 0)
             
-            stock = yf.Ticker(choice)
-            history = stock.history(period="1mo", interval="1d")
-            
-            if history.empty:
-                # Find closest matches
-                stock_symbols = fetch_stock_symbols()
-                close_matches = get_close_matches(choice, stock_symbols)
-                if close_matches:
-                    suggestion_list = "\n".join(close_matches[:3])
-                    embed = disnake.Embed(
-                        title="Stock Symbol Not Found",
-                        description=f"Did you mean: {choice}?\n\nSuggestions:\n{suggestion_list}",
-                        color=disnake.Color.red()
-                    )
-                    await ctx.send(embed=embed)
-                else:
-                    embed = disnake.Embed(
-                        title="No Data Found",
-                        description=f"No data found for {choice}.",
-                        color=disnake.Color.red()
-                    )
-                    await ctx.send(embed=embed)
+            if res['s'] == 'no_data':
+                await ctx.send(f"No data found for {choice}.")
                 return
             
-            current_price = history['Close'][-1]
-            last_month_prices = history['Close']
-            dates = history.index.date
+            # Traitement des données pour créer un graphique
+            # Assurez-vous de traiter les données d'historique des prix en conséquence
             
-            # Create a simple price chart
+            # Créer un graphique
             plt.figure(figsize=(10, 5))
-            plt.plot(dates, last_month_prices, marker='o')
-            plt.xlabel('Date')
-            plt.ylabel('Price')
-            plt.title(f"Stock Price for {choice}")
-            plt.grid(True)
+            # Ajoutez ici le code pour tracer le graphique à partir des données
             
-            # Convert the plot to an image
+            # Convertir le graphique en une image
             image_stream = io.BytesIO()
             plt.savefig(image_stream, format='png')
             plt.close()
             image_stream.seek(0)
             
             embed = disnake.Embed(
-                title=f"Stock Price for {choice}",
-                description=f"Current Price: {current_price:.2f} USD",
+                title=f"Price Chart for {choice}",
                 color=disnake.Color.blurple()
             )
             embed.set_image(url="attachment://chart.png")
-            embed.set_footer(text="Data from Yahoo Finance")
+            embed.set_footer(text="Data from Finnhub")
             
-            msg = await ctx.send(file=disnake.File(fp=image_stream, filename="chart.png"), embed=embed)
-            await ctx.message.delete(delay=5)
-            await msg.delete(delay=60)
+            await ctx.send(file=disnake.File(fp=image_stream, filename="chart.png"), embed=embed)
         
         except Exception as e:
             embed = error.error_embed(e)
