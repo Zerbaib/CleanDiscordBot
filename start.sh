f199aa9b53aa87ffdddd2b221f0aea4fd8146b22
Hennetin Hugo <hugo.hennetin@proton.me>
2023-08-30 00:14:11 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,17 @@
+#bin/bash
+bash
+
 if [ ! -d "bot" ]; then
     git clone https://github.com/Zerbaib/CleanDiscordBot.git bot
     cd bot
     git checkout main
+    python -m venv .venv
     pip install --upgrade pip
     pip install -r requirements.txt
     python main.py
 else
     cd bot
+    source .venv/bin/activate
     git checkout main
     git fetch
     git pull
