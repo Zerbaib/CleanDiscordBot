42fefbe21f997266931c4dc8c8c92c897bd3e4f7
Hennetin Hugo <hugo.hennetin@proton.me>
2023-08-30 00:08:17 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
-#bin/bash
-
-apt install python3-venv -y
-apt install python3-pip -y
-apt install python-is-python3 -y
-
-python -m venv .venv
-source env/bin/activate
-
-git fetch
-git pull
-
-pip install -r requirements.txt
-
-python main.py
+if [ ! -d "bot" ]; then
+    git clone https://github.com/Zerbaib/CleanDiscordBot.git bot
+    cd bot
+    git checkout main
+    pip install --upgrade pip
+    pip install -r requirements.txt
+    python main.py
+else
+    cd bot
+    git checkout main
+    git fetch
+    git pull
+    pip install --upgrade pip
+    pip install -r requirements.txt
+    python main.py
+fi
