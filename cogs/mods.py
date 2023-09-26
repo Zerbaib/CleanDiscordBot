d6f0d19c02f5b1e087e1b3b21db8490c126f81b3
Hugo Hennetin <hugo.hennetin@proton.me>
2023-09-26 15:11:24 +0200

    🔎 debugger

--- cogs/mods.py
+++ cogs/mods.py
@@ -149,24 +149,25 @@ class ModsCommands(commands.Cog):
     @commands.slash_command(name="ban", description="Ban a user from the server")
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, user: disnake.User, reason: str = "No reason provided"):
-        if not ctx.guild.me.permissions.ban_members:
-            embed = disnake.Embed(
-                title="Error",
-                description="I don't have the permission to ban users.",
-                color=disnake.Color.red()
-            )
-            await ctx.send(embed=embed)
         try:
-            await ctx.guild.ban(user, reason=reason)
+            if not ctx.guild.me.permissions.ban_members:
+                embed = disnake.Embed(
+                    title="Error",
+                    description="I don't have the permission to ban users.",
+                    color=disnake.Color.red()
+                )
+                await ctx.send(embed=embed)
+            else:
+                await ctx.guild.ban(user, reason=reason)
 
-            embed = disnake.Embed(
-                title="🔨 User Banned 🔨",
-                description=f"**{user.name}** *aka ``{user.display_name}``* has been banned from the server.",
-                color=disnake.Color.dark_red()
-            )
-            embed.add_field(name="Reason", value=f"`{reason}`")
-            await ctx.response.defer()
-            await ctx.send(embed=embed)
+                embed = disnake.Embed(
+                    title="🔨 User Banned 🔨",
+                    description=f"**{user.name}** *aka ``{user.display_name}``* has been banned from the server.",
+                    color=disnake.Color.dark_red()
+                )
+                embed.add_field(name="Reason", value=f"`{reason}`")
+                await ctx.response.defer()
+                await ctx.send(embed=embed)
 
         except Exception as e:
             embed = error.error_embed(e)
