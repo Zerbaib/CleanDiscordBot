dcca542934a1bda4dcb9e1d103f8f4167ac90fab
Hugo Hennetin <hugo.hennetin@proton.me>
2023-09-28 19:01:42 +0200

    bet in french

--- fr/cogs/casino.py
+++ fr/cogs/casino.py
@@ -94,7 +94,7 @@ class CasinoCommands(commands.Cog):
             embed = error.error_embed(e)
             await ctx.send(embed=embed)
 
-    @commands.slash_command(name="bet", description="Bet coins | x2 or lost")
+    @commands.slash_command(name="bet", description="Le jeux du quitte ou double")
     async def bet(self, ctx, amount: int):
         try:
             user_id = str(ctx.author.id)
@@ -102,32 +102,31 @@ class CasinoCommands(commands.Cog):
                 data = json.load(file)
                 balance = data.get(user_id, 0)
                 if amount <= 0 or amount > balance:
-                    embed = disnake.Embed(title="Invalid Bet Amount", color=disnake.Color.red())
-                    embed.add_field(name="Error", value="Invalid bet amount!")
+                    embed = disnake.Embed(title="Montant de pari invalide", color=disnake.Color.red())
+                    embed.add_field(name="Erreur", value="Montant de pari invalide")
                     await ctx.response.send_message(embed=embed)
                     return
                 if balance < self.min_balance:
-                    embed = disnake.Embed(title="Insufficient Balance", color=disnake.Color.red())
-                    embed.add_field(name="Error", value=f"You need at least {self.min_balance} coins to play!")
+                    embed = disnake.Embed(title="Solde insuffisant", color=disnake.Color.red())
+                    embed.add_field(name="Erreur", value=f"Vous avez besoin d'au moins {self.min_balance} pièces pour jouer !")
                     await ctx.response.send_message(embed=embed)
                     return
 
-                # Game logic: Quitte ou Double
-                win_chance = 25  # 25% chance of winning, 75% chance of losing
+                win_chance = 25
                 outcome = random.choices([True, False], weights=[win_chance, 100 - win_chance], k=1)[0]
 
                 if outcome:
                     winnings = amount * 2
                     data[user_id] += winnings
-                    embed = disnake.Embed(title="💰 You Won!", color=disnake.Color.green())
-                    embed.add_field(name="Outcome", value="Congratulations! You won the bet.", inline=False)
-                    embed.add_field(name="Winnings", value=f"You won `{winnings}` coins!", inline=False)
+                    embed = disnake.Embed(title="💰 Tu as gagné!", color=disnake.Color.green())
+                    embed.add_field(name="Résultat", value="Toutes nos félicitations! Vous avez gagné le pari.", inline=False)
+                    embed.add_field(name="Gains", value=f"Vous avez gagné des pièces `{winnings}` !", inline=False)
                     await ctx.response.defer()
                     await ctx.send(embed=embed)
                 else:
                     data[user_id] -= amount
-                    embed = disnake.Embed(title="😢 You Lost", color=disnake.Color.red())
-                    embed.add_field(name="Outcome", value="Better luck next time. You lost the bet.")
+                    embed = disnake.Embed(title="😢 Tu as perdu", color=disnake.Color.red())
+                    embed.add_field(name="Résultat", value="Plus de chance la prochaine fois. Vous avez perdu le pari.")
                     await ctx.response.defer()
                     await ctx.send(embed=embed)
 
