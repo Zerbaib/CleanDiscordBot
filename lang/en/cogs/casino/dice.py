4a67d220158078c26b23c2cf44095181cef28ced
Hugo Hennetin <hugo.hennetin@proton.me>
2023-10-03 13:15:15 +0200

    🎲 Casino

---
+++ lang/en/cogs/casino/dice.py
@@ -0,0 +1,65 @@
+import json
+import random
+
+import disnake
+from disnake.ext import commands
+
+from lang.en.utils import error
+
+class DiceCommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.data_file = "data/casino.json"
+        
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('🔩 /dice has been loaded')
+
+    @commands.slash_command(name="dice", description="Play the dice game")
+    async def dice(self, ctx, bet: int):
+        try:
+            user_id = str(ctx.author.id)
+            dice_emojis = [':one:', ':two:', ':three:', ':four:', ':five:', ':six:']
+            dice1 = random.randint(1, 6)
+            dice2 = random.randint(1, 6)
+            payout = 0
+            
+            with open(self.data_file, 'r') as file:
+                data = json.load(file)
+                bal = data[user_id]
+            if bet > 0:
+                if bet < bal:
+                    if dice1 == dice2:
+                        payout = bet * dice1
+                        
+                        embed = disnake.Embed(title="🎲 Dice Game 🎲", color=disnake.Color.blue())
+                        embed.add_field(name="Dice Roll Result", value=f"{dice_emojis[dice1 - 1]}  {dice_emojis[dice2 - 1]}", inline=False)
+                        
+                        if payout > 0:
+                            data[user_id] += payout
+                            embed.add_field(name="Result", value=f"You won `{payout}` coin!")
+                            embed.color = disnake.Color.green()
+                        else:
+                            data[user_id] -= bet
+                            embed.add_field(name="Bet", value=f"`{bet}`")
+                            embed.add_field(name="Result", value="You lost your bet.")
+                            embed.color = disnake.Color.red()
+                else:
+                    embed = disnake.Embed(title="You cant play", color=disnake.Color.red())
+                    embed.add_field(name="Error", value="You don't have enough money")
+            else:
+                embed = disnake.Embed(title="You cant play", color=disnake.Color.red())
+                embed.add_field(name="Error", value="You can't play with a negative number")
+
+            await ctx.response.defer()
+            await ctx.send(embed=embed)
+
+            with open(self.data_file, 'w') as file:
+                json.dump(data, file, indent=4)
+        except Exception as e:
+            embed = error.error_embed(e)
+            await ctx.send(embed=embed)
+        
+
+def setup(bot):
+    bot.add_cog(DiceCommand(bot))
