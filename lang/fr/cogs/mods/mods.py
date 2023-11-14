98b41f24542832225d5fb0145746816c030b2ba8
Zerbaib <hugo.hennetin@proton.me>
2023-11-14 23:37:47 +0100

    ➕ unmute

--- lang/fr/cogs/mods/mods.py
+++ lang/fr/cogs/mods/mods.py
@@ -13,35 +13,10 @@ class ModsCommands(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print('🔩 /unmute has been loaded')
         print('🔩 /nick has been loaded')
         print('🔩 /kick has been loaded')
         print('🔩 /ban has been loaded')
         print('🔩 /addemoji has been loaded')
-
-    @commands.slash_command(name="unmute", description="Unmute un membre")
-    @commands.has_permissions(manage_messages=True)
-    async def unmute(self, ctx, member: disnake.Member, reason: str = "Aucune raison spécifiée"):
-        try:
-            with open("config.json", 'r') as config_file:
-                config = json.load(config_file)
-            role_id = config.get("MUTE_ROLE_ID")
-
-            role = disnake.utils.get(ctx.guild.roles, id=role_id)
-            if role and role in member.roles:
-                await member.remove_roles(role)
-
-            embed = disnake.Embed(
-                title="😐 Membre Démué 😐",
-                description=f"{member.mention} a été démuet.",
-                color=disnake.Color.dark_red()
-            )
-            embed.add_field(name="Raison", value=f"```{reason}```")
-            await ctx.response.defer()
-            await ctx.send(embed=embed)
-        except Exception as e:
-            embed = error.error_embed(e)
-            await ctx.send(embed=embed)
 
     @commands.slash_command(name="nick", description="Change le surnom d'un membre")
     async def nick(self, ctx, member: disnake.Member = None, *, nickname: str = None):
---
+++ lang/fr/cogs/mods/unmute.py
@@ -0,0 +1,43 @@
+﻿import json
+
+import aiohttp
+import disnake
+from disnake.ext import commands
+
+from lang.fr.utils import error
+
+
+class UnmuteCommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('🔩 /unmute has been loaded')
+        
+    @commands.slash_command(name="unmute", description="Unmute un membre")
+    @commands.has_permissions(manage_messages=True)
+    async def unmute(self, ctx, member: disnake.Member, reason: str = "Aucune raison spécifiée"):
+        try:
+            with open("config.json", 'r') as config_file:
+                config = json.load(config_file)
+            role_id = config.get("MUTE_ROLE_ID")
+
+            role = disnake.utils.get(ctx.guild.roles, id=role_id)
+            if role and role in member.roles:
+                await member.remove_roles(role)
+
+            embed = disnake.Embed(
+                title="😐 Membre Démué 😐",
+                description=f"{member.mention} a été démuet.",
+                color=disnake.Color.dark_red()
+            )
+            embed.add_field(name="Raison", value=f"```{reason}```")
+            await ctx.response.defer()
+            await ctx.send(embed=embed)
+        except Exception as e:
+            embed = error.error_embed(e)
+            await ctx.send(embed=embed)
+           
+def setup(bot):
+    bot.add_cog(UnmuteCommand(bot))
