915f1abd5bb4b4ff6159c3cdc11fecdbc35363b5
Zerbaib <hugo.hennetin@proton.me>
2023-11-14 16:17:29 +0100

    ➖ unmute in mods.py

--- lang/en/cogs/mods/mods.py
+++ lang/en/cogs/mods/mods.py
@@ -20,30 +20,6 @@ class ModsCommands(commands.Cog):
         print('🔩 /addemoji has been loaded')
         print()
 
-    @commands.slash_command(name="unmute", description="Unmute a member")
-    @commands.has_permissions(manage_messages=True)
-    async def unmute(self, ctx, member: disnake.Member, reason: str = "No reason provided"):
-        try:
-            with open("config.json", 'r') as config_file:
-                config = json.load(config_file)
-            role_id = config.get("MUTE_ROLE_ID")
-            
-            role = disnake.utils.get(ctx.guild.roles, id=role_id)
-            if role and role in member.roles:
-                await member.remove_roles(role)
-
-            embed = disnake.Embed(
-                title="😐 Member Unmuted 😐",
-                description=f"{member.mention} has been unmuted.",
-                color=disnake.Color.dark_red()
-            )
-            embed.add_field(name="Reason", value=f"```{reason}```")
-            await ctx.response.defer()
-            await ctx.send(embed=embed)
-        except Exception as e:
-            embed = error.error_embed(e)
-            await ctx.send(embed=embed)
-
     @commands.slash_command(name="nick", description="Change the nickname of a member")
     async def nick(self, ctx, member: disnake.Member = None, *, nickname: str = None):
         try:
