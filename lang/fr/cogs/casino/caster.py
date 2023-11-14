45197cc9d9204ebf2f46e321d9a88cdb96076cd1
Zerbaib <hugo.hennetin@proton.me>
2023-11-14 21:24:54 +0100

    ➕ caster + slot

--- lang/fr/cogs/casino/caster.py
+++ lang/fr/cogs/casino/caster.py
@@ -10,7 +10,7 @@ from lang.fr.utils import error
 
 cooldown_time = 60 * 60 * 2
 
-class CasinoCommands(commands.Cog):
+class CasterCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.data_file = "data/casino.json"
@@ -28,14 +28,11 @@ class CasinoCommands(commands.Cog):
             "pair": 2,
             "impair": 2
         }
-
+        
     @commands.Cog.listener()
     async def on_ready(self):
-        print('========== ⚙️ Casino ⚙️ ==========')
         print('🔩 /caster has been loaded')
-        print('🔩 /slot has been loaded')
-        print()
-
+        
     @commands.slash_command(name="caster", description="Jouez à la roulette")
     async def caster(self, ctx, bet_option: str, bet_amount: int):
         try:
@@ -100,93 +97,6 @@ class CasinoCommands(commands.Cog):
         except Exception as e:
             embed = error.error_embed(e)
             await ctx.send(embed=embed)
-
-    @commands.slash_command(name='slot', description='Jouez à la machine à sous')
-    async def slot(self, ctx, bet: int):
-        try:
-            if bet <= 0:
-                embed = disnake.Embed(
-                    title="Machine à sous",
-                    description="La mise doit être supérieure à 0.",
-                    color=disnake.Color.red()
-                )
-                await ctx.send(embed=embed)
-                return
-
-            with open('data/casino.json', 'r') as file:
-                data = json.load(file)
-
-            user_id = str(ctx.author.id)
-            if user_id not in data:
-                embed = disnake.Embed(
-                    title="Machine à sous",
-                    description="Vous n'avez pas de compte de casino. Utilisez ``/earn`` pour gagner des pièces.",
-                    color=disnake.Color.red()
-                )
-                await ctx.send(embed=embed)
-                return
-
-            balance = data[user_id]
-            if balance < bet:
-                embed = disnake.Embed(
-                    title="Machine à sous",
-                    description="Solde insuffisant.\nVous n'avez pas assez de pièces pour placer ce pari.",
-                    color=disnake.Color.red()
-                )
-                await ctx.send(embed=embed)
-                return
-
-            reels = ["🍒", "🍊", "🍋", "🍇", "🔔", "💎", "🍀", "🍎"]
-            random.shuffle(reels)
             
-            result = []
-            for _ in range(3):
-                symbol = random.choice(reels)
-                result.append(symbol)
-            random.shuffle(reels)
-            ligne1 = []
-            for _ in range(3):
-                symbol = random.choice(reels)
-                ligne1.append(symbol)
-            ligne2 = []
-            for _ in range(3):
-                symbol = random.choice(reels)
-                ligne2.append(symbol)
-            embed = disnake.Embed(title="🎰 Machine à sous 🎰", color=disnake.Color.blurple())
-            embed.add_field(name="Reels",
-                            value=f"| ``{ligne1[0]} | {ligne1[1]} | {ligne1[2]}`` |\n\n"
-                                f"**>** **``{result[0]} | {result[1]} | {result[2]}``** **<**\n\n"
-                                f"| ``{ligne2[0]} | {ligne2[1]} | {ligne2[2]}`` |",
-                            inline=False
-                            )
-
-            if result[0] == result[1] == result[2]:
-                win_amount = bet * 10 
-                balance += win_amount
-                embed.add_field(name="Résultat",
-                                value=f"Bien joué! Vous avez obtenu 3 symboles identiques et gagné **``{win_amount}``** pièces !",
-                                inline=False
-                                )
-            else:
-                balance -= bet
-                embed.add_field(name="Résultat",
-                                value="Désolé, vous n'avez pas gagné cette fois.",
-                                inline=False
-                                )
-
-            data[user_id] = balance
-
-            with open('data/casino.json', 'w') as file:
-                json.dump(data, file, indent=4)
-
-            embed.add_field(name="Solde",
-                            value=f"Il vous reste **``{balance}``** pièces.",
-                            inline=False
-                            )
-            await ctx.send(embed=embed)
-        except Exception as e:
-            embed = error.error_embed(e)
-            await ctx.send(embed=embed)
-
 def setup(bot):
-    bot.add_cog(CasinoCommands(bot))
+    bot.add_cog(CasterCommand(bot))
