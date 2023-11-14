d1cefbb46390c113838b24b1ff8c797d96925712
Zerbaib <hugo.hennetin@proton.me>
2023-11-14 22:28:24 +0100

    ➕ pay + baltop

---
+++ lang/fr/cogs/eco/baltop.py
@@ -0,0 +1,42 @@
+﻿import json
+
+import disnake
+from disnake.ext import commands
+
+from lang.fr.utils import error
+
+
+class BaltopCommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.data_file = "data/casino.json"
+        
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('🔩 /baltop has been loaded')
+        
+    @commands.slash_command(name="baltop", description="Top 10 des utilisateurs les plus riches")
+    async def baltop(self, ctx):
+        try:
+            with open(self.data_file, 'r') as file:
+                data = json.load(file)
+
+            sorted_data = sorted(data.items(), key=lambda item: item[1], reverse=True)
+            top_users = sorted_data[:10]
+
+            embed = disnake.Embed(title="💰 Top 10 des plus riches 💰", color=disnake.Color.blurple())
+            for idx, (user_id, balance) in enumerate(top_users, start=1):
+                user = self.bot.get_user(int(user_id))
+                if user:
+                    embed.add_field(name=f"{idx}. {user.display_name}", value=f"Solde: `{balance}` pieces", inline=False)
+                else:
+                    embed.add_field(name=f"{idx}. Utilisateur pas trouver", value=f"Solde: `{balance}` pieces", inline=False)
+
+            await ctx.response.defer()
+            await ctx.send(embed=embed)
+        except Exception as e:
+            embed = error.error_embed(e)
+            await ctx.send(embed=embed)
+            
+def setup(bot):
+    bot.add_cog(BaltopCommand(bot))
--- lang/fr/cogs/eco/eco.py
+++ lang/fr/cogs/eco/eco.py
@@ -6,40 +6,14 @@ from disnake.ext import commands
 from lang.fr.utils import error
 
 
-class EconomyCommands(commands.Cog):
+class PayCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.data_file = "data/casino.json"
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print('========== ⚙️ Economy ⚙️ ==========')
-        print('🔩 /baltop has been loaded')
         print('🔩 /pay has been loaded')
-        print()
-
-    @commands.slash_command(name="baltop", description="Top 10 des utilisateurs les plus riches")
-    async def baltop(self, ctx):
-        try:
-            with open(self.data_file, 'r') as file:
-                data = json.load(file)
-
-            sorted_data = sorted(data.items(), key=lambda item: item[1], reverse=True)
-            top_users = sorted_data[:10]
-
-            embed = disnake.Embed(title="💰 Top 10 des plus riches 💰", color=disnake.Color.blurple())
-            for idx, (user_id, balance) in enumerate(top_users, start=1):
-                user = self.bot.get_user(int(user_id))
-                if user:
-                    embed.add_field(name=f"{idx}. {user.display_name}", value=f"Solde: `{balance}` pieces", inline=False)
-                else:
-                    embed.add_field(name=f"{idx}. Utilisateur pas trouver", value=f"Solde: `{balance}` pieces", inline=False)
-
-            await ctx.response.defer()
-            await ctx.send(embed=embed)
-        except Exception as e:
-            embed = error.error_embed(e)
-            await ctx.send(embed=embed)
 
     @commands.slash_command(name="pay", description="Paye un utilisateur")
     async def pay(self, ctx, amount: int, user: disnake.Member):
@@ -106,4 +80,4 @@ class EconomyCommands(commands.Cog):
             await ctx.send(embed=embed)
 
 def setup(bot):
-    bot.add_cog(EconomyCommands(bot))
+    bot.add_cog(PayCommand(bot))
