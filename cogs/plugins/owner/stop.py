2d130c3aa8525700ffb64a47ea9b83e569a045ed
Hugo Hennetin <hugo.hennetin@proton.me>
2024-04-10 11:47:09 +0200

    stop format

--- cogs/plugins/owner/stop.py
+++ cogs/plugins/owner/stop.py
@@ -3,6 +3,9 @@ from disnake.ext import commands
 from cogs.utils import error
 from cogs.utils.color import hex_to_discord_color
 from cogs.utils.embed import create_embed
+from cogs.utils.lang_loader import load_owner_lang
+
+langText = load_owner_lang()
 
 
 class StopCommand(commands.Cog):
@@ -13,11 +16,11 @@ class StopCommand(commands.Cog):
     async def on_ready(self):
         print('🔩 /stop has been loaded')
 
-    @commands.slash_command(name="stop", description="Stop the bot")
+    @commands.slash_command(name="stop", description=langText.get("STOP_DESCRIPTION"))
     @commands.is_owner()
     async def stop(self, ctx):
         try:
-            await ctx.send("🛑 Stopping the bot... 🛑", ephemeral=True)
+            await ctx.send(langText.get("STOP_TITLE"), ephemeral=True)
             await self.bot.close()
         except Exception as e:
             embed = error.error_embed(e)
